8562d519564c3eb9c44e8e56b6481079bbf2f198
CloudNiner <andrew@cloudniner.co>
2016-05-20 11:33:09 -0400

    Add views/serializers for ClimateModel and City

--- django/climate_change_api/climate_data/serializers.py
+++ django/climate_change_api/climate_data/serializers.py
@@ -1,9 +1,26 @@
 from rest_framework import serializers
 
-from climate_data.models import ClimateData
+from climate_data.models import City, ClimateData, ClimateModel
+
+
+class CitySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = City
+        fields = ()
 
 
 class ClimateDataSerializer(serializers.ModelSerializer):
 
+    city = serializers.HyperlinkedRelatedField(read_only=True, view_name='city-detail')
+    climate_model = serializers.SlugRelatedField(read_only=True, slug_field='name')
+
     class Meta:
         model = ClimateData
+
+
+class ClimateModelSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ClimateModel
+        fields = ('name',)
--- django/climate_change_api/climate_data/views.py
+++ django/climate_change_api/climate_data/views.py
@@ -1,7 +1,17 @@
 from rest_framework import filters, viewsets
 
-from climate_data.models import ClimateData
-from climate_data.serializers import ClimateDataSerializer
+from climate_data.models import City, ClimateData, ClimateModel
+from climate_data.serializers import (CitySerializer,
+                                      ClimateDataSerializer,
+                                      ClimateModelSerializer)
+
+
+class CityViewSet(viewsets.ReadOnlyModelViewSet):
+
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+    filter_backends = (filters.DjangoFilterBackend, filters.OrderingFilter,)
+    ordering_fields = ('name',)
 
 
 class ClimateDataViewSet(viewsets.ReadOnlyModelViewSet):
@@ -10,3 +20,11 @@ class ClimateDataViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = ClimateDataSerializer
     filter_backends = (filters.DjangoFilterBackend, filters.OrderingFilter,)
     ordering_fields = ('city', 'climate_model', 'year', 'day_of_year', 'tasmin', 'tasmax', 'pr',)
+
+
+class ClimateModelViewSet(viewsets.ReadOnlyModelViewSet):
+
+    queryset = ClimateModel.objects.all()
+    serializer_class = ClimateModelSerializer
+    filter_backends = (filters.DjangoFilterBackend, filters.OrderingFilter,)
+    ordering_fields = ('name',)
