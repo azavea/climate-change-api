5405927df748c39e9e27cc29a9a05c1761e7b2c8
CloudNiner <andrew@cloudniner.co>
2016-08-16 15:45:26 -0400

    Calculate yearly temp indicators

--- django/climate_change_api/indicators/models.py
+++ django/climate_change_api/indicators/models.py
@@ -1,3 +1,5 @@
+from django.db.models import Avg
+
 from climate_data.models import ClimateData
 
 class Indicator(object):
@@ -21,45 +23,52 @@ class Indicator(object):
     def _validate_years(self, year_list):
         return year_list
 
-    @property
-    def key(self):
-        return 'INDICATOR -- override in subclass'
-
-    @property
-    def label(self):
-        return 'Generic Indicator -- override in subclass'
-
-    @property
-    def description(self):
-        return 'Generic description -- override in subclass'
-
     def filter_objects(self):
         """ A subclass can override this to further filter the dataset before calling calculate """
         return self.queryset
 
     def calculate(self):
-        """ Calculate the indicator """
+        """ Calculate the indicator
+
+        This method should use self.queryset to calculate the indicator returning a dict
+        of the form:
+        {
+            'year': value
+        }
+        in the case of yearly aggregated indicators, and
+        {
+            'year': [jan_value, feb_value,...,dec_value]
+        }
+        in the case of monthly aggregated indicators
+
+        """
         raise NotImplementedError('Subclasses must implement calculate()')
 
 
-class YearlyAverageMaxTemperature(Indicator):
+class YearlyIndicator(Indicator):
 
-    @property
-    def key(self):
-        return 'YEARLY_AVG_MAX_TEMP'
+    def serialize(self, results, varname):
+        output = {}
+        for result in results:
+            output[result['data_source__year']] = result[varname]
+        return output
 
-    @property
-    def label(self):
-        return 'Yearly Average Max Temperature'
 
-    @property
-    def description(self):
-        return ''
+class YearlyAverageMaxTemperature(YearlyIndicator):
 
     def calculate(self):
-        return {}
+        results = self.queryset.values('data_source__year').annotate(tasmax=Avg('tasmax'))
+        return self.serialize(results, 'tasmax')
+
+
+class YearlyAverageMinTemperature(YearlyIndicator):
+
+    def calculate(self):
+        results = self.queryset.values('data_source__year').annotate(tasmin=Avg('tasmin'))
+        return self.serialize(results, 'tasmin')
 
 
 INDICATOR_MAP = {
     'yearly_avg_max_temp': YearlyAverageMaxTemperature,
+    'yearly_avg_min_temp': YearlyAverageMinTemperature,
 }
