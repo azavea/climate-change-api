25ad4f54e6de60ec2d985ce0a8e00365c5bd8dda
CloudNiner <andrew@cloudniner.co>
2016-08-17 16:57:43 -0400

    Refactor Indicator tests
    Should make adding additional test cases pretty straightforward
    
    Might be a better way to structure the classes so that each
    derived TestCase class doesn't need to inherit from two classes?

--- django/climate_change_api/indicators/tests/test_indicators.py
+++ django/climate_change_api/indicators/tests/test_indicators.py
@@ -4,66 +4,58 @@ from climate_data.tests.mixins import ClimateDataSetupMixin
 from indicators import indicators
 
 
-class YearlyAverageMaxTemperatureTestCase(ClimateDataSetupMixin, TestCase):
+class IndicatorTests(ClimateDataSetupMixin, object):
+
+    indicator_class = None    # Override this in subclass to set the indicator to test
+    test_indicator_no_data_equals = {}
+    test_indicator_rcp85_equals = None
+    test_indicator_rcp45_equals = None
+    test_years_filter_equals = None
+    test_models_filter_equals = None
 
     def test_indicator_rcp85(self):
-        indicator = indicators.YearlyAverageMaxTemperature(self.city1, self.rcp85)
+        indicator = self.indicator_class(self.city1, self.rcp85)
         data = indicator.calculate()
-        self.assertEqual(data, {2000: 35.0})
+        self.assertEqual(data, self.test_indicator_rcp85_equals)
 
     def test_indicator_rcp45(self):
-        indicator = indicators.YearlyAverageMaxTemperature(self.city1, self.rcp45)
+        indicator = self.indicator_class(self.city1, self.rcp45)
         data = indicator.calculate()
-        self.assertEqual(data, {2000: 15.0, 2001: 15.0, 2002: 10.0, 2003: 10.0})
+        self.assertEqual(data, self.test_indicator_rcp45_equals)
 
     def test_indicator_no_data(self):
-        indicator = indicators.YearlyAverageMaxTemperature(self.city2, self.rcp85)
+        indicator = self.indicator_class(self.city2, self.rcp85)
         data = indicator.calculate()
-        self.assertEqual(data, {})
+        self.assertEqual(data, self.test_indicator_no_data_equals)
 
     def test_years_filter(self):
         indicator = indicators.YearlyAverageMaxTemperature(self.city1,
                                                            self.rcp45,
                                                            years='2001:2002')
         data = indicator.calculate()
-        self.assertEqual(data, {2001: 15.0, 2002: 10.0})
+        self.assertEqual(data, self.test_years_filter_equals)
 
     def test_models_filter(self):
         indicator = indicators.YearlyAverageMaxTemperature(self.city1,
                                                            self.rcp45,
                                                            models='model1')
         data = indicator.calculate()
-        self.assertEqual(data, {2000: 10.0, 2001: 10.0, 2002: 10.0, 2003: 10.0})
+        self.assertEqual(data, self.test_models_filter_equals)
 
 
-class YearlyAverageMinTemperatureTestCase(ClimateDataSetupMixin, TestCase):
+class YearlyAverageMaxTemperatureTestCase(IndicatorTests, TestCase):
 
-    def test_indicator_rcp85(self):
+    indicator_class = indicators.YearlyAverageMaxTemperature
+    test_indicator_rcp85_equals = {2000: 35.0}
+    test_indicator_rcp45_equals = {2000: 15.0, 2001: 15.0, 2002: 10.0, 2003: 10.0}
+    test_years_filter_equals = {2001: 15.0, 2002: 10.0}
+    test_models_filter_equals = {2000: 10.0, 2001: 10.0, 2002: 10.0, 2003: 10.0}
 
-        indicator = indicators.YearlyAverageMinTemperature(self.city1, self.rcp85)
-        data = indicator.calculate()
-        self.assertEqual(data, {2000: 35.0})
 
-    def test_indicator_rcp45(self):
-        indicator = indicators.YearlyAverageMinTemperature(self.city1, self.rcp45)
-        data = indicator.calculate()
-        self.assertEqual(data, {2000: 15.0, 2001: 15.0, 2002: 10.0, 2003: 10.0})
+class YearlyAverageMinTemperatureTestCase(IndicatorTests, TestCase):
 
-    def test_indicator_no_data(self):
-        indicator = indicators.YearlyAverageMinTemperature(self.city2, self.rcp85)
-        data = indicator.calculate()
-        self.assertEqual(data, {})
-
-    def test_years_filter(self):
-        indicator = indicators.YearlyAverageMinTemperature(self.city1,
-                                                           self.rcp45,
-                                                           years='2001:2002')
-        data = indicator.calculate()
-        self.assertEqual(data, {2001: 15.0, 2002: 10.0})
-
-    def test_models_filter(self):
-        indicator = indicators.YearlyAverageMinTemperature(self.city1,
-                                                           self.rcp45,
-                                                           models='model1')
-        data = indicator.calculate()
-        self.assertEqual(data, {2000: 10.0, 2001: 10.0, 2002: 10.0, 2003: 10.0})
+    indicator_class = indicators.YearlyAverageMinTemperature
+    test_indicator_rcp85_equals = {2000: 35.0}
+    test_indicator_rcp45_equals = {2000: 15.0, 2001: 15.0, 2002: 10.0, 2003: 10.0}
+    test_years_filter_equals = {2001: 15.0, 2002: 10.0}
+    test_models_filter_equals = {2000: 10.0, 2001: 10.0, 2002: 10.0, 2003: 10.0}
