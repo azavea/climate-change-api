9ed8d65644393c3e6947e9d7a7baeef554f0a1b7
CloudNiner <andrew@cloudniner.co>
2016-08-17 15:42:13 -0400

    Add docstrings to indicators.serializers

--- django/climate_change_api/indicators/serializers.py
+++ django/climate_change_api/indicators/serializers.py
@@ -1,7 +1,19 @@
 
 class IndicatorSerializer(object):
+    """ Serialize a django queryset result to a list of dictionaries
+    of the form:
+        {
+            'year': value
+        }
+    in the case of yearly aggregated indicators, and
+        {
+            'year': [jan_value, feb_value,...,dec_value]
+        }
+    in the case of monthly aggregated indicators
 
+    """
     def to_representation(self, results):
+        """ Override this method with indicator specific requirements for serialization """
         return results
 
 
