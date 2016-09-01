eaf5f7c8ea3de892b5479d675854865337a1f04d
Kathryn Killebrew <kkillebrew@azavea.com>
2016-09-01 17:14:15 -0400

    Add temperature conversion test
    Test converting Kelvin to Fahrenheit.

--- django/climate_change_api/indicators/tests/test_indicators.py
+++ django/climate_change_api/indicators/tests/test_indicators.py
@@ -14,6 +14,7 @@ class IndicatorTests(ClimateDataSetupMixin, object):
     test_indicator_rcp45_equals = None
     test_years_filter_equals = None
     test_models_filter_equals = None
+    test_units_fahrenheit_equals = None
 
     def test_indicator_name(self):
         self.assertEqual(self.indicator_class.name(), self.indicator_name)
@@ -50,6 +51,12 @@ class IndicatorTests(ClimateDataSetupMixin, object):
         data = indicator.calculate()
         self.assertEqual(data, self.test_models_filter_equals)
 
+    def test_unit_conversion(self):
+        indicator = self.indicator_class(self.city1, self.rcp85, units='F')
+        data = indicator.calculate()
+        self.assertEqual(data, self.test_units_fahrenheit_equals,
+                         'Temperature should be converted to degrees F')
+
 
 class YearlyAverageMaxTemperatureTestCase(IndicatorTests, TestCase):
 
@@ -59,6 +66,7 @@ class YearlyAverageMaxTemperatureTestCase(IndicatorTests, TestCase):
     test_indicator_rcp45_equals = {2000: 15.0, 2001: 15.0, 2002: 10.0, 2003: 10.0}
     test_years_filter_equals = {2001: 15.0, 2002: 10.0}
     test_models_filter_equals = {2000: 10.0, 2001: 10.0, 2002: 10.0, 2003: 10.0}
+    test_units_fahrenheit_equals = {2000: -396.67}
 
 
 class YearlyAverageMinTemperatureTestCase(IndicatorTests, TestCase):
@@ -69,3 +77,4 @@ class YearlyAverageMinTemperatureTestCase(IndicatorTests, TestCase):
     test_indicator_rcp45_equals = {2000: 15.0, 2001: 15.0, 2002: 10.0, 2003: 10.0}
     test_years_filter_equals = {2001: 15.0, 2002: 10.0}
     test_models_filter_equals = {2000: 10.0, 2001: 10.0, 2002: 10.0, 2003: 10.0}
+    test_units_fahrenheit_equals = {2000: -396.67}
