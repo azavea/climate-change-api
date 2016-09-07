b87638094f503b825325dae5ef29fcf1f49b835a
Reed Martz <rmartz@azavea.com>
2016-09-07 10:29:07 -0400

    Convert daily serialization to use generators
    This helps avoid re-processing the same data multiple times, and means
    that we do not need to maintain copies of the data in memory until we
    build the final structure.

--- django/climate_change_api/indicators/serializers.py
+++ django/climate_change_api/indicators/serializers.py
@@ -11,16 +11,18 @@ class IndicatorSerializer(object):
 class DailyIndicatorSerializer(IndicatorSerializer):
 
     def to_representation(self, results):
+        # Convert the timeseries data into a more easily digestible tuple format
+        # The list is already sorted, so we can just maintain the original order
+        tuple_list = ((d['data_source__year'], d['day_of_year'], d['value'])
+                     for d in results if d['value'])
 
-        # Organize the list by day of year
-        flat_list = [(d['data_source__year'], d['day_of_year'], d['value'])
-                     for d in results if d['value']]
-        flat_list.sort(key=lambda (year, day_of_year, value): day_of_year)
-        years = set([year for (year, _, _) in flat_list])
+        # Use ordered dicts to guarantee output order
+        output = OrderedDict()
+        for (year, day_of_year, value) in tuple_list:
+            if year not in output:
+                output[year] = OrderedDict()
 
-        # Use an ordered dictionary to keep response sorted
-        output = {year: OrderedDict() for year in years}
-        for (year, day_of_year, value) in flat_list:
+            # Convert year and day_of_year to a Python date object
             day = date(year, 1, 1) + timedelta(days=day_of_year-1)
             output[year][day.isoformat()] = value
         return output
