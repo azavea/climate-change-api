dec5201648d776a96cc1963967527f6873e88579
Reed Martz <rmartz@azavea.com>
2016-09-26 09:47:39 -0400

    Abstract result collating to base indicator class
    Aggregation levels now just need to implement a method that defines the
    key for each row, leaving the boilerplate collating logic to the parent
    class.

--- django/climate_change_api/indicators/serializers.py
+++ django/climate_change_api/indicators/serializers.py
@@ -1,4 +1,36 @@
+def float_avg(values):
+    return float(sum(values)) / len(values)
+
+
 class IndicatorSerializer(object):
     def to_representation(self, results):
-        """ Override this method with indicator specific requirements for serialization """
-        return results
+        """ Simplify the full list of collated data points to a constant summary
+
+        Given the results of `aggregate`, should produce a dictionary of the form:
+        {
+            'time_repr': {'avg': value, 'min': value, 'max': value}
+        }
+
+        Where 'time_repr' is the date in a hyphen-deliminated ISO-8601 format for
+        the appropriate aggregation level. Specifically, one of the following:
+        * YYYY for yearly data
+        * YYYY-MM for monthly data
+        * YYYY-MM-DD for daily data
+
+        _Do not_ use YYYYMMDD
+
+        For example, a yearly indicator could be presented as:
+        {
+            '2050': {'avg': value, 'min': value, 'max': value}
+        }
+
+        And a monthly indicator as:
+        {
+            '2050-03': {'avg': value, 'min': value, 'max': value}
+        }
+
+        """
+        return {key: {'avg': float_avg(values),
+                      'min': min(values),
+                      'max': max(values)}
+                for (key, values) in results.iteritems()}
