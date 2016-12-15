8b279636722a6bd7a5187a97fb6577058f097e28
Reed Martz <rmartz@azavea.com>
2016-12-15 17:46:30 -0500

    Abstract unit conversion tests so all combinations are tested automatically

--- django/climate_change_api/indicators/tests/test_unit_converters.py
+++ django/climate_change_api/indicators/tests/test_unit_converters.py
@@ -5,45 +5,21 @@ from indicators.unit_converters import TemperatureConverter, PrecipitationConver
 
 class TemperatureConverterTestCase(ConverterTestMixin, TestCase):
     converter_class = TemperatureConverter
-    cases = {
-        ('F', 'K'): [
-            (0, 255.372222),
-            (32, 273.15),
-            (73.45, 296.17778)
-        ],
-        ('K', 'F'): [
-            (0, -459.67),
-            (276.3, 37.67)
-        ],
-        ('K', 'C'): [
-            (0, -273.15),
-            (295.3, 22.15)
-        ],
-        ('C', 'F'): [
-            (-40, -40),
-            (0, 32),
-            (10, 50),
-            (100, 212),
-            (23.2, 73.76)
-        ],
-        ('F', 'C'): [
-            (80, 26.6667),
-            (73.2, 22.888889),
-            (-40, -40),
-            (0, -17.77778),
-            (212, 100),
-            (32, 0)
-        ]
-    }
+    cases = [
+        {'F': -459.67, 'C': -273.15, 'K': 0},
+        {'F': -40, 'C': -40, 'K': 233.15},
+        {'F': 0, 'K': 255.372222, 'C': -17.77778},
+        {'F': 32, 'C': 0, 'K': 273.15},
+        {'F': 37.67, 'K': 276.3, 'C': 3.15},
+        {'F': 71.87, 'C': 22.15, 'K': 295.3},
+        {'F': 73.45, 'C': 23.027778, 'K': 296.17778},
+        {'F': 73.76, 'C': 23.2, 'K': 296.35},
+        {'F': 212, 'C': 100, 'K': 373.15}
+    ]
 
 
 class PrecipitationConverterTestCase(ConverterTestMixin, TestCase):
     converter_class = PrecipitationConverter
-    cases = {
-        ('kg/m^2/s', 'kg/m^2/day'): [
-            (1, 86400)
-        ],
-        ('kg/m^2/day', 'kg/m^2/s'): [
-            (86400, 1)
-        ]
-    }
+    cases = [
+        {'kg/m^2/s': 1, 'kg/m^2/day': 86400}
+    ]
