5bcb8f848d4937d98039400def19d3c8270a2ba2
Reed Martz <rmartz@azavea.com>
2016-12-15 17:46:30 -0500

    Abstract unit conversion tests so all combinations are tested automatically

--- django/climate_change_api/indicators/tests/mixins.py
+++ django/climate_change_api/indicators/tests/mixins.py
@@ -1,11 +1,17 @@
+from itertools import permutations
+
+
 class ConverterTestMixin():
     def test_conversions(self):
-        for units, tests in self.cases.iteritems():
-            converter = self.converter_class.get(*units)
+        for case in self.cases:
+            # Test all possible combinations and ensure that they all convert properly
+            for units, values in (zip(*v) for v in permutations(case.items(), 2)):
+                converter = self.converter_class.get(*units)
 
-            for start, expected in tests:
-                value = converter(start)
-                self.assertAlmostEqual(value, expected,
+                start, expected = values
+                actual = converter(start)
+
+                self.assertAlmostEqual(actual, expected,
                                        places=3,
                                        msg='Failed assertion that %f %s = %f %s, got %f %s' %
-                                       (start, units[0], expected, units[1], value, units[1]))
+                                       (start, units[0], expected, units[1], actual, units[1]))
