78cf1b70d1561f8c83ff8413276098010ead91b4
CloudNiner <andrew@cloudniner.co>
2017-01-04 20:47:35 -0500

    Add throttling to climate data and indicator endpoints
    For now, we only throttle the climate data and indicator data
    retrieval endpoints as they are the only resource intensive
    endpoints.
    
    We'll likely need to tweak the rate limits set here, these are
    only a first pass attempt to get throttling working.

---
+++ django/climate_change_api/climate_change_api/throttling.py
@@ -0,0 +1,22 @@
+from django.core.cache import caches
+
+from rest_framework.throttling import UserRateThrottle
+
+
+class ClimateDataRateThrottle(UserRateThrottle):
+    """ Override to always use the throttling specific cache backend """
+    cache = caches['api_throttling']
+
+
+class ClimateDataBurstRateThrottle(ClimateDataRateThrottle):
+    """ Set a relatively low 'burst' rate limit, data queries are relatively expensive """
+    rate = '10/min'
+
+
+class ClimateDataSustainedRateThrottle(ClimateDataRateThrottle):
+    """ Set a relatively high max daily rate throttle
+
+    Not too concerned about users pulling data slowly over long time periods
+
+    """
+    rate = '5000/day'
