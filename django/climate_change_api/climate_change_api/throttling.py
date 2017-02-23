06a1db870843c77c4e62ccbfd54b7ebcf4e1ab07
CloudNiner <andrew@cloudniner.co>
2017-02-23 10:30:44 -0500

    Refactor allow_request into UserCustomRateThrottle superclass

--- django/climate_change_api/climate_change_api/throttling.py
+++ django/climate_change_api/climate_change_api/throttling.py
@@ -3,7 +3,23 @@ from django.core.cache import caches
 from rest_framework.throttling import UserRateThrottle
 
 
-class ClimateDataRateThrottle(UserRateThrottle):
+class UserCustomRateThrottle(UserRateThrottle):
+    """ Allow setting custom user throttle rates defined on the ClimateUser model.
+    Defaults to
+
+    `model_rate_field`: Specify throttle type on child class. Required to override default."""
+
+    def allow_request(self, request, view):
+        if request.user is not None:
+            user_rate = getattr(request.user, self.model_rate_field, self.rate)
+            if self.rate != user_rate:
+                self.rate = user_rate
+                self.num_requests, self.duration = self.parse_rate(self.rate)
+
+        return super(UserCustomRateThrottle, self).allow_request(request, view)
+
+
+class ClimateDataRateThrottle(UserCustomRateThrottle):
     """ Override to always use the throttling specific cache backend """
     cache = caches['api_throttling']
 
@@ -11,13 +27,7 @@ class ClimateDataRateThrottle(UserRateThrottle):
 class ClimateDataBurstRateThrottle(ClimateDataRateThrottle):
     """ Set a relatively low 'burst' rate limit, data queries are relatively expensive """
     scope = 'burst'
-
-    def allow_request(self, request, view):
-        if self.rate != request.user.burst_rate:
-            self.rate = request.user.burst_rate
-            self.num_requests, self.duration = self.parse_rate(self.rate)
-
-        return super(ClimateDataBurstRateThrottle, self).allow_request(request, view)
+    model_rate_field = 'burst_rate'
 
 
 class ClimateDataSustainedRateThrottle(ClimateDataRateThrottle):
@@ -27,10 +37,4 @@ class ClimateDataSustainedRateThrottle(ClimateDataRateThrottle):
 
     """
     scope = 'sustained'
-
-    def allow_request(self, request, view):
-        if self.rate != request.user.sustained_rate:
-            self.rate = request.user.sustained_rate
-            self.num_requests, self.duration = self.parse_rate(self.rate)
-
-        return super(ClimateDataSustainedRateThrottle, self).allow_request(request, view)
+    model_rate_field = 'sustained_rate'
