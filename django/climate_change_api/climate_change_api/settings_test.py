b2c47f718571a24276cbe4e76941196503ec5163
Kathryn Killebrew <kkillebrew@azavea.com>
2017-02-27 14:57:37 -0500

    Add cache alias to test configuration
    Fixes tests.

--- django/climate_change_api/climate_change_api/settings_test.py
+++ django/climate_change_api/climate_change_api/settings_test.py
@@ -4,6 +4,9 @@ CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
     },
+    'bypass': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    },
     'api_views': {
         'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
     },
