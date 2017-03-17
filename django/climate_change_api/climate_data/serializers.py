5ebfb75edca5f1de83bf1151a4610d7ce9b2c318
Kathryn Killebrew <kkillebrew@azavea.com>
2017-03-17 11:43:05 -0400

    Linting
    Break up assert statement.

--- django/climate_change_api/climate_data/serializers.py
+++ django/climate_change_api/climate_data/serializers.py
@@ -100,7 +100,8 @@ class ClimateCityScenarioDataSerializer(serializers.BaseSerializer):
             ORDER BY year, day_of_year;
 
         """
-        assert isinstance(queryset, QuerySet), 'ClimateCityScenarioDataSerializer must be given a queryset'  # NOQA: E501
+        assert isinstance(queryset, QuerySet), (
+            'ClimateCityScenarioDataSerializer must be given a queryset')
 
         aggregation = self._context['aggregation']
         aggregation_function = getattr(django.db.models, aggregation.capitalize())
