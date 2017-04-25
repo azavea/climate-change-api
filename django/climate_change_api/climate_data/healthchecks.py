f776e292fe41e6b95bfeb88a748b064b36b0fe39
Jenny Fung <jfung@azavea.com>
2017-04-25 15:38:47 -0400

    Set year ranges as constants

--- django/climate_change_api/climate_data/healthchecks.py
+++ django/climate_change_api/climate_data/healthchecks.py
@@ -3,8 +3,6 @@
 import itertools
 import logging
 
-from django.db.models import Count
-
 from climate_data.models import (City,
                                  ClimateDataSource,
                                  ClimateModel,
@@ -12,6 +10,9 @@ from climate_data.models import (City,
 
 
 logger = logging.getLogger(__name__)
+
+HISTORIC_YEARS = range(1950, 2006)
+FUTURE_YEARS = range(2006, 2101)
 
 
 def check_city_cells():
@@ -40,17 +41,17 @@ def check_missing_data_sources():
     source for that year, model, or scenario.
     """
     distinct_models = ClimateDataSource.objects.distinct('model').values_list('model', flat=True)
-    historical_scenario = Scenario.objects.filter(name='historical').values_list('id', flat=True)
+    historical_scenarios = Scenario.objects.filter(name='historical').values_list('id', flat=True)
     future_scenarios = (ClimateDataSource.objects.distinct('scenario')
-                                                 .exclude(scenario=historical_scenario)
+                                                 .exclude(scenario__in=historical_scenarios)
                                                  .values_list('scenario', flat=True))
 
     # convert years to strings because ints are not iterable
-    historical_years = [str(x) for x in set(range(1950, 2006))]
-    future_years = [str(x) for x in set(range(2006, 2101))]
+    historical_years_str = [str(x) for x in HISTORIC_YEARS]
+    future_years_str = [str(x) for x in FUTURE_YEARS]
 
-    expected_historic = len(distinct_models) * len(historical_scenario) * len(historical_years)
-    expected_future = len(distinct_models) * len(future_scenarios) * len(future_years)
+    expected_historic = len(distinct_models) * len(historical_scenarios) * len(historical_years_str)
+    expected_future = len(distinct_models) * len(future_scenarios) * len(future_years_str)
     expected_total = expected_historic + expected_future
 
     # first do a simple check of counts to see if anything is missing
@@ -60,11 +61,11 @@ def check_missing_data_sources():
     # If got this far, at least one expected data source is not present. Find them all.
     # expect to see a source for every combination of each year, model, and scenario present
     expected_set = set()
-    [expected_set.add(tuple(x)) for x in itertools.product(historical_years,
+    [expected_set.add(tuple(x)) for x in itertools.product(historical_years_str,
                                                            distinct_models,
-                                                           historical_scenario)]
+                                                           historical_scenarios)]
 
-    [expected_set.add(tuple(x)) for x in itertools.product(future_years,
+    [expected_set.add(tuple(x)) for x in itertools.product(future_years_str,
                                                            distinct_models,
                                                            future_scenarios)]
 
@@ -72,7 +73,7 @@ def check_missing_data_sources():
     expected_set = set((int(item[0]), item[1], item[2]) for item in expected_set)
 
     # every year/model/scenario for which there is a source
-    source_set = ClimateDataSource.objects.values_list('year', 'model', 'scenario')
+    source_set = set(ClimateDataSource.objects.values_list('year', 'model', 'scenario'))
 
     # will output set of tuples of missing year/model/scenario sources
     missing = expected_set.difference(source_set)
