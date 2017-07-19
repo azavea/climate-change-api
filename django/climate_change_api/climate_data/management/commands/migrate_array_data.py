c998770eb7468a8d51fe58fd19a64c4b6bde0bbb
Kathryn Killebrew <kkillebrew@azavea.com>
2017-07-19 16:08:55 -0400

    Use day of year when creating data arrays
    When populating ClimateDataYear data arrays, use day of year for index offset.
    Models with no leap day data will then have a null value for day 60.
    Fixes #574.

--- django/climate_change_api/climate_data/management/commands/migrate_array_data.py
+++ django/climate_change_api/climate_data/management/commands/migrate_array_data.py
@@ -4,6 +4,7 @@ from itertools import groupby
 from climate_data.models import ClimateData, ClimateDataYear
 
 from django.db import IntegrityError
+from django.db.models import Max
 from django.core.management.base import BaseCommand
 
 logger = logging.getLogger(__name__)
@@ -15,17 +16,30 @@ class Command(BaseCommand):
     help = 'Migrates data from daily ClimateData to yearly ClimateDataYear'
 
     def calculate_yearly_sets(self, queryset):
+        # get last day of year for this model/year
+        max_days = queryset.aggregate(Max('day_of_year'))['day_of_year__max']
+
         # Loop over all ClimateData data points, grouped by map cell and data source
         all_data = queryset.values().order_by('map_cell', 'data_source', 'day_of_year')
         for (cell, source), data in groupby(all_data.iterator(),
                                             lambda r: (r['map_cell_id'], r['data_source_id'])):
             data = list(data)
+
+            tasmin = [None] * max_days
+            tasmax = [None] * max_days
+            pr = [None] * max_days
+
+            for datum in data:
+                tasmin[datum['day_of_year']-1] = datum['tasmin']
+                tasmax[datum['day_of_year']-1] = datum['tasmax']
+                pr[datum['day_of_year']-1] = datum['pr']
+
             yield ClimateDataYear(
                 map_cell_id=cell,
                 data_source_id=source,
-                tasmin=[x['tasmin'] for x in data],
-                tasmax=[x['tasmax'] for x in data],
-                pr=[x['pr'] for x in data]
+                tasmin=tasmin,
+                tasmax=tasmax,
+                pr=pr
             )
 
     def handle(self, *args, **options):
