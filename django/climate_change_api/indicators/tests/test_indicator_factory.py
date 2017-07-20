a8506a8ef4092851c33ae1fbe2687f9a8f208064
Kathryn Killebrew <kkillebrew@azavea.com>
2017-07-20 11:41:55 -0400

    Use feature falg in test
    Fixes #577. Allow tests to be run with CC_FF_ARRAY_DATA feature flag enabled.

--- django/climate_change_api/indicators/tests/test_indicator_factory.py
+++ django/climate_change_api/indicators/tests/test_indicator_factory.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.test import TestCase
 
 from indicators import indicator_factory
@@ -7,7 +8,11 @@ class IndicatorFactoryTestCase(TestCase):
 
     def test_indicator_factory_returns_class(self):
         IndicatorClass = indicator_factory('average_high_temperature')
-        self.assertEqual(IndicatorClass.__name__, 'AverageHighTemperature')
+        if settings.FEATURE_FLAGS['array_data']:
+            self.assertEqual(IndicatorClass.__name__, 'AverageHighTemperatureArray')
+        else:
+            # TODO: remove this block and feature flag test with task #567
+            self.assertEqual(IndicatorClass.__name__, 'AverageHighTemperature')
 
     def test_indicator_factory_invalid_argument(self):
         IndicatorClass = indicator_factory('does_not_exist')
