1260cc17b70474b6168f6d9803fc08c049283eef
Reed Martz <rmartz@azavea.com>
2017-08-23 09:45:02 -0400

    Add reverse migration for removing City.map_cell

--- django/climate_change_api/climate_data/migrations/0054_remove_city_map_cell.py
+++ django/climate_change_api/climate_data/migrations/0054_remove_city_map_cell.py
@@ -7,11 +7,21 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
 
+    def migrate_restore_city_cell_foreign_key(apps, schema_editor):
+        """If we reverse this migration, set City.map_cell to the city's NEX-GDDP cell."""
+        City = apps.get_model('climate_data', 'City')
+        city_data = (City.objects.all().values('id', 'cell_set__cell_id')
+                     .filter(cell_set__dataset__name='NEX-GDDP'))
+
+        for id, cell_id in city_data:
+            City.objects.filter(id=id).update(map_cell=cell_id)
+
     dependencies = [
         ('climate_data', '0053_migrate_city_cells'),
     ]
 
     operations = [
+        migrations.RunPython(migrations.RunPython.noop, migrate_restore_city_cell_foreign_key),
         migrations.RemoveField(
             model_name='city',
             name='map_cell',
