9b45584d4c09cffc6bfcac1e3214d6224456efcc
Reed Martz <rmartz@azavea.com>
2017-08-23 10:47:48 -0400

    Change code to attempt to use new M2M City<->MapCell relation

--- django/climate_change_api/climate_data/tests/test_models.py
+++ django/climate_change_api/climate_data/tests/test_models.py
@@ -9,9 +9,9 @@ class ModelsTestCase(ClimateDataSetupMixin, TestCase):
     def test_delete_data_cell_does_not_delete_associated_city(self):
         self.assertEqual(City.objects.count(), 2)
         self.assertEqual(self.city1, City.objects.get(id=self.city1.id))
-        self.assertEqual(self.city1.map_cell, self.mapcell)
+        self.assertEqual(self.city1.cell_set.get(), self.mapcell)
         count = ClimateDataCell.objects.count()
 
         self.mapcell.delete()
-        self.assertIsNone(self.city1.map_cell.id)
+        self.assertIsEmpty(self.city1.cell_set.all())
         self.assertEqual(ClimateDataCell.objects.count(), count - 1)
