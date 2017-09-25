cf1008a9bcdf6209d8d3020be3bd82e80212295f
CloudNiner <andrew@cloudniner.co>
2017-09-25 12:46:33 -0400

    Fixup precip indicator tests changed to rates

--- django/climate_change_api/indicators/unit_converters.py
+++ django/climate_change_api/indicators/unit_converters.py
@@ -85,6 +85,7 @@ class PrecipitationRateConverter(LinearConverter):
     units = {
         'kg/m^2/s': 1,
         'kg/m^2/day': SECONDS_PER_DAY,
+        'in/s': INCHES_PER_MILLIMETER,
         'in/day': INCHES_PER_MILLIMETER * SECONDS_PER_DAY,
     }
 
