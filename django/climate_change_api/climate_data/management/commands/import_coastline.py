7ec8bda6000b7ba2002b7fd61614962d80db9e04
Kathryn Killebrew <kkillebrew@azavea.com>
2017-11-13 21:08:11 -0500

    Save Coastline via management commmand
    Load Coastline line strings to Django from NOAA source.

--- django/climate_change_api/climate_data/management/commands/import_coastline.py
+++ django/climate_change_api/climate_data/management/commands/import_coastline.py
@@ -5,14 +5,16 @@ from esridump.dumper import EsriDumper
 
 from django.core.management.base import BaseCommand
 
-#from climate_data.models import City, Region
-#from django.contrib.gis.geos import GEOSGeometry, MultiPolygon
+from django.contrib.gis.geos import GEOSGeometry, LineString
+
+from climate_data.models import Coastline
 
 logger = logging.getLogger(__name__)
 
 
 # ArcGIS server hosting NOAA coastline data
 COAST_URL = 'https://encdirect.noaa.gov/arcgis/rest/services/encdirect/enc_coastal/MapServer/70'
+COASTAL_SRID = 4326
 
 
 class Command(BaseCommand):
@@ -22,10 +24,14 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         logger.info('Running command handler. TODO: import coastline')
 
-        d = EsriDumper(COAST_URL)
-        print(d)
+        dumper = EsriDumper(COAST_URL)
 
-        for feature in d:
-            print(json.dumps(feature))
+        for feature in dumper:
+            object_id = feature['properties']['OBJECTID']
+            line_string = LineString(feature['geometry']['coordinates'],
+                                     srid=COASTAL_SRID).simplify(0.001)
+
+            Coastline.objects.update_or_create(geom=line_string, objectid=object_id)
+            logger.info('Loaded coastline LineString: %s', object_id)
 
         logger.info('Got shoreline features downloaded')
