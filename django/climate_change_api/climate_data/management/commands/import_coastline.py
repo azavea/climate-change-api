5a8ad339740dea1d64b77e0d53a83fc2a9b122de
Kathryn Killebrew <kkillebrew@azavea.com>
2017-11-14 10:13:15 -0500

    Linting

--- django/climate_change_api/climate_data/management/commands/import_coastline.py
+++ django/climate_change_api/climate_data/management/commands/import_coastline.py
@@ -1,11 +1,11 @@
 import logging
 
-import json
 from esridump.dumper import EsriDumper
 
 from django.core.management.base import BaseCommand
 
-from django.contrib.gis.geos import GEOSGeometry, LineString
+# from django.contrib.gis.db.models import Union
+from django.contrib.gis.geos import LineString
 
 from climate_data.models import Coastline
 
@@ -18,20 +18,29 @@ COASTAL_SRID = 4326
 
 
 class Command(BaseCommand):
-    """Imports coast outline from NOAA.
-    """
+    """Imports coast outline from NOAA."""
 
     def handle(self, *args, **options):
-        logger.info('Running command handler. TODO: import coastline')
+        logger.info('Running NOAA coastline import')
 
         dumper = EsriDumper(COAST_URL)
 
         for feature in dumper:
             object_id = feature['properties']['OBJECTID']
             line_string = LineString(feature['geometry']['coordinates'],
-                                     srid=COASTAL_SRID).simplify(0.001)
+                                     srid=COASTAL_SRID)
 
-            Coastline.objects.update_or_create(geom=line_string, objectid=object_id)
+            Coastline.objects.update_or_create(objectid=object_id, defaults={'geom': line_string})
             logger.info('Loaded coastline LineString: %s', object_id)
 
         logger.info('Got shoreline features downloaded')
+
+        # TODO: bounds check with cities
+
+        # multi_line = Coastline.objects.aggregate(Collect('geom'))['geom__collect']
+        # buff = multi_line.buffer(0.0001).unary_union
+        # coastal_cities = CityBoundary.objects.filter(geom__intersects=buff)
+        # get invalid geoms
+        # bad = Coastline.objects.filter(geom__isvalid=False)
+        # Too few points in geometry component
+        # with .simplify(0.001)
