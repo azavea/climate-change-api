5a920d9be618cfdf2a1a54b8548ac893e02cabe8
Kathryn Killebrew <kkillebrew@azavea.com>
2017-11-15 11:53:36 -0500

    Fix city boundary geometries
    Rewrite city boundary importer to use pyesridump library to query ArcGIS server and parse response.
    Fix geometry self-intersections with buffer to zero (similar to ST_MakeValid).
    
    Fixes #762.

--- django/climate_change_api/climate_data/geo_boundary/census.py
+++ django/climate_change_api/climate_data/geo_boundary/census.py
@@ -1,14 +1,27 @@
 from collections import OrderedDict
+import json
 import logging
 
 import requests
 from retry import retry
 
-from django.contrib.gis.geos import MultiPolygon, Polygon
+from esridump.dumper import EsriDumper
+
+from django.contrib.gis.geos import GEOSGeometry, Polygon, MultiPolygon
 
 from .exceptions import GeoBoundaryError
 
 logger = logging.getLogger(__name__)
+
+
+CENSUS_MAP_SERVER_URL_TEMPLATE = ('https://tigerweb.geo.census.gov/arcgis/rest/services' +
+                                  '/TIGERweb/tigerWMS_Current/MapServer/{layer_id}')
+
+CENSUS_LAYERS = OrderedDict([
+    ('Incorporated Places', 28),
+    ('Census Designated Places', 30),
+    ('Counties', 86),
+])
 
 
 class CensusBoundaryError(GeoBoundaryError):
@@ -45,29 +58,6 @@ def geocoder_api_request(return_type, search_type, params=None):
     return data['result']
 
 
-@retry((requests.exceptions.ConnectionError,), tries=3, delay=1, backoff=2)
-def map_server_request(layer_id, params=None):
-    """Make a request to the Census Map Server.
-
-    Returns the map server query response directly.
-    Forces use of the f=pjson query param.
-
-    :param layer_id: int - One of the layer ids at
-        https://tigerweb.geo.census.gov/arcgis/rest/services/TIGERweb/tigerWMS_Current/MapServer
-    :param params: dict - Query params to pass to API request.
-    """
-    if params is None:
-        params = {}
-
-    CENSUS_MAP_SERVER_URL_TEMPLATE = ('https://tigerweb.geo.census.gov/arcgis/rest/services' +
-                                      '/TIGERweb/tigerWMS_Current/MapServer/{layer_id}/query')
-    url = CENSUS_MAP_SERVER_URL_TEMPLATE.format(layer_id=layer_id)
-
-    params['f'] = 'pjson'
-    response = requests.get(url, params=params)
-    return response.json()
-
-
 def boundary_from_point(point, srid=4326):
     """Return an appropriate boundary from the Census API for a given Point.
 
@@ -81,29 +71,40 @@ def boundary_from_point(point, srid=4326):
     :param srid: int - Desired SRID of the MultiPolygon boundary. Default: 4326.
     :return (boundary: MultiPolygon, boundary_type: str). The associated boundary and its type.
     """
+
+    if not point.x and not point.y:
+        raise CensusBoundaryError("Cannot get census boundary for missing Point: {}".format(point))
+
     def geom_from_census_boundary(boundary, layer_id, srid=4326):
         params = {
-            'where': 'OID={}'.format(boundary['OID']),
-            'returnGeometry': 'true',
-            'outSr': srid,
-            'f': 'pjson',
+            'where': 'OID={}'.format(boundary['OID'])
         }
-        results = map_server_request(layer_id, params=params)
-        features = results['features']
-        geometry_type = results['geometryType']
-        # ArcGIS MapServer doesn't have a multipolygon type so only check for Polygon
-        if len(features) > 0 and geometry_type == 'esriGeometryPolygon':
-            feature = features[0]
-            rings = feature['geometry']['rings']
-            geom = Polygon(*rings, srid=srid)
-            return MultiPolygon(geom)
-        return None
+        dumper = EsriDumper(CENSUS_MAP_SERVER_URL_TEMPLATE.format(layer_id=layer_id),
+                            extra_query_args=params)
 
-    CENSUS_LAYERS = OrderedDict([
-        ('Incorporated Places', 28),
-        ('Census Designated Places', 30),
-        ('Counties', 86),
-    ])
+        # return a multipolygon that is the union of the polygons in the returned features
+        return_poly = None
+        for x in dumper:
+            geom = GEOSGeometry(json.dumps(x['geometry']), srid=srid)
+            if not return_poly:
+                return_poly = geom
+            else:
+                return_poly = return_poly.union(geom)
+        if not return_poly.valid:
+            logger.warn('Fixing Census boundary polygon for OID {}'.format(boundary['OID']))
+            # buffering to zero can fix some geometry issues, such as ring self-intersections
+            return_poly = return_poly.buffer(0)
+
+        # wrap Polygon objects in a MultiPolygon, for consistent return type
+        if isinstance(return_poly, Polygon):
+            return_poly = MultiPolygon(return_poly)
+
+        if return_poly.valid:
+            return return_poly
+        else:
+            # might occur if attempting to fix by buffering above failed
+            logger.exception('Census boundary polygon invalid for OID {}'.format(boundary['OID']))
+            return None
 
     params = {
         'benchmark': 'Public_AR_Current',
