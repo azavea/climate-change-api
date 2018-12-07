ced801dd8e322cf788b8314869f382b270f25b99
CloudNiner <andy@finkmiller.com>
2018-12-07 11:19:19 -0500

    Add GeoJson, Geonames and OurAirport location sources
    Allow both shpfile and geojson debug files to be written
    
    Files written with these location sources were saved
    to s3://azavea-climate-sandbox so they can be pulled down
    by the importer later.

--- django/climate_change_api/climate_data/nex2db/location_sources.py
+++ django/climate_change_api/climate_data/nex2db/location_sources.py
@@ -5,6 +5,8 @@ New sources should define functions or callable classes that perform this mappin
 """
 
 from collections import namedtuple
+import csv
+import json
 import math
 import os
 import shutil
@@ -42,21 +44,39 @@ def download_file(url, local_filename=None):
     return local_filename
 
 
-def write_debug_shapefile(climate_locations, output_filename):
-    """Write a shapefile of Points from the given list of ClimateLocation tuples."""
-    with fiona.open(output_filename, 'w',
-                    driver='ESRI Shapefile',
-                    crs=from_epsg(4326),
-                    schema={'geometry': 'Point', 'properties': []}) as shapefile:
-        features = [{
-            'type': 'Feature',
-            'geometry': {
-                'type': 'Point',
-                'coordinates': [l.x, l.y],
-            },
-            'properties': {},
-        } for l in climate_locations]
-        shapefile.writerecords(features)
+def write_debug_file(climate_locations, output_filename, file_format='geojson'):
+    FORMAT_OPTIONS = ('geojson', 'shpfile',)
+    if file_format not in FORMAT_OPTIONS:
+        raise TypeError('write_debug_file file_format must be one of {}'.format(FORMAT_OPTIONS))
+
+    features = [{
+        'type': 'Feature',
+        'geometry': {
+            'type': 'Point',
+            'coordinates': [l.x, l.y],
+        },
+        'properties': {
+            'id': l.id,
+            'name': l.name,
+        },
+    } for l in climate_locations]
+
+    if file_format == 'geojson':
+        geojson = {
+            'type': 'FeatureCollection',
+            'features': features
+        }
+        with open(output_filename, 'w') as jsonfile:
+            json.dump(geojson, jsonfile, indent=2)
+    elif file_format == 'shpfile':
+        with fiona.open(output_filename, 'w',
+                        driver='ESRI Shapefile',
+                        crs=from_epsg(4326),
+                        schema={'geometry': 'Point', 'properties': {'id': 'str', 'name': 'str'}}) as shapefile:
+            shapefile.writerecords(features)
+    else:
+        raise TypeError('file_format handler not available for {}'.format(file_format))
+
     print('Features written to: {}'.format(output_filename))
 
 
@@ -64,6 +84,75 @@ def ClimateAPICityLocationSource(city_queryset, dataset):
     """Generate list of ClimateLocation cells from a climate_data.models.City queryset."""
     return [ClimateLocation(id=city.id, x=city.geom.x, y=city.geom.y, name=city.name)
             for city in city_queryset]
+
+
+def GeoJsonUrlLocationSource(geojson_url):
+    locations = []
+    tmpdir = tempfile.mkdtemp()
+    try:
+        json_filename = os.path.join(tmpdir, '{}.json'.format(str(uuid.uuid4())))
+        download_file(geojson_url, local_filename=json_filename)
+        with open(json_filename, 'r') as data:
+            feature_collection = json.load(data)
+            locations = [
+                ClimateLocation(id=uuid.uuid4(),
+                                name='',
+                                x=float(feature['geometry']['coordinates'][0]),
+                                y=float(feature['geometry']['coordinates'][1]))
+                for feature in feature_collection['features']
+            ]
+    finally:
+        shutil.rmtree(tmpdir, ignore_errors=True)
+    return locations
+
+
+def GeonamesLocationSource(geonames_file, dataset, filter_fn=None):
+    """Generate list of ClimateLocation cells from a geonames text file.
+
+    Optionally filter each row in the file by filter_fn. filter_fn takes a single arg which
+    is a dict of row data where keys are values in GEONAMES_HEADER. It must return a boolean, true
+    if row should be included in output, otherwise false.
+
+    Input text file should be one of the files described here:
+
+    http://download.geonames.org/export/dump/readme.txt
+
+    that conforms to the 'geoname' table specification.
+
+    """
+    GEONAMES_HEADER = [
+        'geonameid',
+        'name',
+        'asciiname',
+        'alternatenames',
+        'latitude',
+        'longitude',
+        'featureclass',
+        'featurecode',
+        'countrycode',
+        'cc2',
+        'admin1code',
+        'admin2code',
+        'admin3code',
+        'admin4code',
+        'population',
+        'elevation',
+        'dem',
+        'timezone',
+        'modificationdate',
+    ]
+
+    locations = []
+    if filter_fn is None:
+        filter_fn = lambda row: True
+    with open(geonames_file, 'r') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter='\t', fieldnames=GEONAMES_HEADER)
+        locations = [
+            ClimateLocation(id=row['geonameid'], name=['asciiname'],
+                            x=float(row['longitude']), y=float(row['latitude']))
+            for row in reader if filter_fn(row)
+        ]
+    return locations
 
 
 def MultipolygonBoundaryLocationSource(shapefile_url, dataset):
@@ -124,3 +213,37 @@ def MultipolygonBoundaryLocationSource(shapefile_url, dataset):
                             name='({}, {})'.format(c[0], c[1]),
                             x=c[0],
                             y=c[1]) for c in lonlat_coords]
+
+
+def OurAirportsLocationSource():
+    """Generate list of ClimateLocation cells from a OurAirports airports.csv.
+
+    Filters csv to only US airports
+
+    http://ourairports.com/data/
+
+    """
+    def filter_fn(row):
+        AIRPORT_TYPES = ['small_airport', 'medium_airport', 'large_airport']
+        if row['iso_country'].strip() == 'US' and row['type'].strip() in AIRPORT_TYPES:
+            return True
+        else:
+            return False
+
+    # Airports CSV cached in S3 for https and output consistency, the source file is updated daily
+    download_url = 'https://s3.amazonaws.com/azavea-climate-sandbox/airports.csv'
+    tmpdir = tempfile.mkdtemp()
+    locations = []
+    try:
+        csv_filename = os.path.join(tmpdir, 'airports.csv')
+        download_file(download_url, local_filename=csv_filename)
+        with open(csv_filename, 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            locations = [
+                ClimateLocation(id=row['ident'], name=row['name'],
+                                x=float(row['longitude_deg']), y=float(row['latitude_deg']))
+                for row in reader if filter_fn(row)
+            ]
+    finally:
+        shutil.rmtree(tmpdir, ignore_errors=True)
+    return locations
