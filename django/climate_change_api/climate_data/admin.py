18b4a5966227b82223c51c2fd3ce84d76b56b5ff
Michael Maurizi <mmaurizi@azavea.com>
2019-04-05 15:23:51 -0400

    Fix some TODOs
    I tried the suggestion of switching to .union() but that made tests
    fail due to different semantics, so I decided to leave it as-is.

--- django/climate_change_api/climate_data/admin.py
+++ django/climate_change_api/climate_data/admin.py
@@ -4,9 +4,6 @@ from climate_data.models import City
 
 
 class CityAdmin(admin.GeoModelAdmin):
-    # TODO (GH #411): This can be removed when upgrading to Django 1.11
-    openlayers_url = 'https://cdnjs.cloudflare.com/ajax/libs/openlayers/2.13.1/OpenLayers.js'
-
     exclude = ('_geog',)
 
 
